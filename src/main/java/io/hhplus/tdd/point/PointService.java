84a8af444a66d10cea57627b09b507818d5ec7ef
JunHyeok Seo <sjhgd107@naver.com>
2024-09-25 21:53:05 +0900

    feat : 조회(포인트, 히스토리) 기능 구현 - repository의 값을 그대로 전달하는 서비스 : 단위테스트 불필요

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -5,11 +5,21 @@ import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PointService {
 	private final UserPointTable    userPointTable;
 	private final PointHistoryTable pointHistoryTable;
 
+	//유저 포인트 조회
+	public UserPoint getPointById(long id) {
+		return userPointTable.selectById(id);
+	}
 
+	//유저 포인트 히스토리 조회
+	public List<PointHistory> getPointHistoryById(long id) {
+		return pointHistoryTable.selectAllByUserId(id);
+	}
 }
