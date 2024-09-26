49bb396780eb1dfe6816e8257899700dd1a0818c
JunHyeok Seo <sjhgd107@naver.com>
2024-09-27 03:50:52 +0900

    feat : 동시성 처리 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,48 +1,64 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.LockManager;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.Lock;
+
 @Service
 @RequiredArgsConstructor
 public class PointService {
-	private final UserPointTable    userPointTable;
+	private final UserPointTable userPointTable;
+	private final LockManager lockManager;
 
 	/**
 	 * 포인트 충전
 	 * */
-	public UserPoint chargePoint(long id, long amount) {
-		if (amount < 1)
-			throw new IllegalArgumentException("포인트 충전 금액이 1보다 작습니다");
+	public UserPoint charge(long id, long amount) {
+		Lock lock = lockManager.getLock(id);
+		lock.lock();
+		try {
+			if (amount < 1)
+				throw new IllegalArgumentException("포인트 충전 금액은 1 이상이어야 합니다");
 
-		UserPoint userPoint   = userPointTable.selectById(id);
-		long currentPoint     = userPoint.point();
-		long updatedPoint     = currentPoint + amount;
+			UserPoint userPoint   = userPointTable.selectById(id);
+			long currentPoint     = userPoint.point();
+			long updatedPoint     = currentPoint + amount;
 
-		return userPointTable.insertOrUpdate(id, updatedPoint);
+			return userPointTable.insertOrUpdate(id, updatedPoint);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	/**
 	 * 포인트 사용
 	 * */
-	public UserPoint usePoint(long id, long amount) {
-		if (amount < 1)
-			throw new IllegalArgumentException("포인트 사용 금액이 1보다 작습니다");
+	public UserPoint use(long id, long amount) {
+		Lock lock = lockManager.getLock(id);
+		lock.lock();
+		try {
+			if (amount < 1)
+				throw new IllegalArgumentException("포인트 사용 금액은 1 이상이어야 합니다");
 
-		UserPoint userPoint   = userPointTable.selectById(id);
-		long currentPoint     = userPoint.point();
+			UserPoint userPoint = userPointTable.selectById(id);
+			long currentPoint = userPoint.point();
 
-		if (currentPoint < amount)
-			throw new IllegalStateException("포인트가 부족합니다");
+			if (currentPoint < amount)
+				throw new IllegalStateException("사용 가능한 포인트가 부족합니다");
 
-		long updatedPoint     = currentPoint - amount;
+			long updatedPoint = currentPoint - amount;
 
-		return userPointTable.insertOrUpdate(id, updatedPoint);
+			return userPointTable.insertOrUpdate(id, updatedPoint);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	/**
 	 * 유저 포인트 조회
 	 * */
-	public UserPoint selectUserPoint(long id) { return userPointTable.selectById(id); }
+	public UserPoint selectPoint(long id) { return userPointTable.selectById(id); }
 }
